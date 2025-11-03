7384025c4441cb41f4fb5029a896c85bce5b1c36
benjamin10ks <bjkmanning@gmail.com>
2025-11-03 00:29:58 -0700

    schemas and validation

--- apps/server/src/utils/auth.validators.js
+++ apps/server/src/utils/auth.validators.js
@@ -1,5 +1,18 @@
-export const loginSchema = {};
+import Joi from "joi";
 
-export const registerSchema = {};
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
 
-export const convertGuestSchema = {};
+export const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().min(3).max(30).required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const convertGuestSchema = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().min(3).max(30).required(),
+  password: Joi.string().min(6).required(),
+});
